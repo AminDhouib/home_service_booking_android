473cdbd324f9c05438f9b8c7ab8cb7ef91042f1b
Jasmine Zribi <JZRIB059@uottawa.ca>
2018-10-20 13:48:55 -0400

    added intent for singup and initialized textviews and editviews for LoginActivity and UserAccountActivity

--- app/src/main/java/com/handy/agile/agile_app/UserAccountActivity.java
+++ app/src/main/java/com/handy/agile/agile_app/UserAccountActivity.java
@@ -2,6 +2,10 @@ package com.handy.agile.agile_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class UserAccountActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class UserAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account);
+
+        final TextView tvUsername = (TextView) findViewById(R.id.tvUsername);
+        final TextView tvRole = (TextView) findViewById(R.id.tvRole);
+        final TextView tvWelcome = (TextView) findViewById(R.id.tvWelcome);
+        final TextView tvLoggedInAs = (TextView) findViewById(R.id.tvLoggedInAs);
     }
 }
