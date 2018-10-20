a17c10d7af089f6033a851f187bedf0c0c7f6bbb
Gregory Ivo <gregorybivo@gmail.com>
2018-10-20 12:26:31 -0400

    added Sign

---
+++ app/src/main/java/com/handy/agile/agile_app/signUpActivity.java
@@ -0,0 +1,14 @@
+package com.handy.agile.agile_app;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class signUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
