a2e1ea48ce2c41ddfe4907c74cfe70b51b7b3174
Jasmine Zribi <JZRIB059@uottawa.ca>
2018-10-20 13:33:09 -0400

    started working on the LoginActivity java code

--- app/src/main/java/com/handy/agile/agile_app/LoginActivity.java
+++ app/src/main/java/com/handy/agile/agile_app/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.handy.agile.agile_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bSignUp = (Button) findViewById(R.id.bSingUp);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+
+        bSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent = new Intent(LoginActivity.this,signUpActivity.class);
+            }
+        });
+
     }
+
+
 }
