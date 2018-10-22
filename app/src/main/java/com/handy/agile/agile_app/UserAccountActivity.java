87a505ec37c89bc2c90c313c85166077696988f1
Jasmine Zribi <JZRIB059@uottawa.ca>
2018-10-22 17:51:24 -0400

    Admin only should be able to see the list of users that have accounts

--- app/src/main/java/com/handy/agile/agile_app/UserAccountActivity.java
+++ app/src/main/java/com/handy/agile/agile_app/UserAccountActivity.java
@@ -1,30 +1,98 @@
 package com.handy.agile.agile_app;
 
 import android.content.Intent;
+import android.opengl.Visibility;
+import android.support.annotation.NonNull;
+import android.support.constraint.solver.widgets.Snapshot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserAccountActivity extends AppCompatActivity {
 
     TextView tvUsername;
     TextView tvRole;
 
+    //list of users in the DB
+    ListView listViewUsers;
+
+    DatabaseReference databaseUser;
+
+    List<User> users;
+
+    User user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        User user = (User)intent.getSerializableExtra("User");
+        user = (User)intent.getSerializableExtra("User");
         setContentView(R.layout.activity_user_account);
 
         tvUsername = (TextView) findViewById(R.id.tvUsername);
         tvUsername.setText("Welcome, " + user.getName());
         tvRole = (TextView) findViewById(R.id.tvRole);
-        tvRole.setText("Role: " + user.getRole());
+        tvRole.setText("You are logged in as " + user.getRole());
+
+        listViewUsers = (ListView) findViewById(R.id.listViewUsers);
+        users = new ArrayList<>();
+
+        databaseUser = FirebaseDatabase.getInstance().getReference();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //if you're logged in as admin
+        if (user.getRole().equals("Admin")) {
+            listViewUsers.setVisibility(View.VISIBLE);
+            displayDataBase();
+        }
+
+
+
+    }
+
+    //Display database if Admin
+    private void displayDataBase() {
+        databaseUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                users.clear();
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class);
+
+                    users.add(user);
+                }
+                UserList userAdapter = new UserList(UserAccountActivity.this,users);
+                listViewUsers.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
+
---
+++ app/src/main/java/com/handy/agile/agile_app/UserList.java
@@ -0,0 +1,55 @@
+package com.handy.agile.agile_app;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class UserList extends ArrayAdapter<User> {
+
+
+    private Activity context;
+    private List<User> users;
+
+    public UserList(Activity context, List<User> users) {
+        super(context, R.layout.user_list, users);
+        this.context = context;
+        this.users = users;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.user_list, null, true);
+
+        TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
+        TextView textViewLastName = (TextView)  listViewItem.findViewById(R.id.textViewLastName);
+        TextView textViewEmail = (TextView) listViewItem.findViewById(R.id.textViewEmail);
+        TextView textViewAddress = (TextView) listViewItem.findViewById(R.id.textViewAddress);
+        TextView textViewPhoneNumber = (TextView) listViewItem.findViewById(R.id.textViewPhoneNumber);
+        TextView textViewRole = (TextView) listViewItem.findViewById(R.id.textViewRole);
+
+
+        User user = users.get(position);
+        textViewName.setText(user.getName()+" ");
+        textViewLastName.setText(user.getLastName());
+        textViewEmail.setText(user.getEmail());
+        textViewAddress.setText(user.getAddress());
+        textViewPhoneNumber.setText(user.getPhoneNumber());
+        textViewRole.setText(user.getRole());
+
+
+
+        return listViewItem;
+    }
+}
