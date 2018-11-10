9e8bf93a158d6d00ccb0017296dc8301cbee217e
Gregory Ivo <gregorybivo@gmail.com>
2018-11-10 15:55:44 -0500

    String Representation For Currencies!

--- app/src/main/java/com/handy/agile/agile_app/ServiceList.java
+++ app/src/main/java/com/handy/agile/agile_app/ServiceList.java
@@ -17,7 +17,9 @@ import android.support.v4.app.Fragment;
 
 import org.w3c.dom.Text;
 
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class ServiceList extends ArrayAdapter<Service> {
 
@@ -42,7 +44,14 @@ public class ServiceList extends ArrayAdapter<Service> {
 
         Service service = services.get(position);
         textViewServiceType.setText(service.getType());
-        textViewHourlyRate.setText(Float.toString(service.getHourlyRate()));
+
+        //Changes here to format AS a currency
+        Locale locale = Locale.US;
+        NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+        String currencyText = fmt.format(service.getHourlyRate());
+
+
+        textViewHourlyRate.setText(currencyText);
 
         //Set on click listener to the 'edit' button of each
         listViewItem.findViewById(R.id.editServiceBtn).setOnClickListener(new View.OnClickListener() {
@@ -52,9 +61,13 @@ public class ServiceList extends ArrayAdapter<Service> {
                 EditServiceDialog dialog = new EditServiceDialog();
                 Bundle bundle = new Bundle();
 
-                //Pass values to tthe dialog
+                //Pass values to the dialog
                 bundle.putString("Service", (String)textViewServiceType.getText());
-                bundle.putString("HourlyRate", (String)textViewHourlyRate.getText());
+
+                //removing text elements from numeric string so it can be easily edited.
+                String tempHourlyRate = (String)textViewHourlyRate.getText();
+                tempHourlyRate = tempHourlyRate.replace("$","").replace(",", "");
+                bundle.putString("HourlyRate", tempHourlyRate);
                 dialog.setArguments(bundle);
 
                 //Show the Dialog
