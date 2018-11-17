04b918153a1438922805775c876902dde8f35caa
Jasmine Zribi <JZRIB059@uottawa.ca>
2018-11-16 21:16:44 -0500

    Added the 7 days of the week and the add button (not yet implemented(

--- app/src/main/java/com/handy/agile/agile_app/ServiceSearchActivity.java
+++ app/src/main/java/com/handy/agile/agile_app/ServiceSearchActivity.java
@@ -19,6 +19,7 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,7 +29,13 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ServiceSearchActivity extends AppCompatActivity {
@@ -45,6 +52,10 @@ public class ServiceSearchActivity extends AppCompatActivity {
 
     User user;
     Service service;
+
+    TimePickerDialog timePickerDialog;
+
+
 
 
 
@@ -122,22 +133,99 @@ public class ServiceSearchActivity extends AppCompatActivity {
         final AlertDialog b = dialogBuilder.create();
         b.show();
 
-        final  TextView monday = dialogView.findViewById(R.id.monday);
-        //I want to make it so that clicking  on this will prop a time picker
-        final TextView mondayStartTime = dialogView.findViewById(R.id.mondayStartTime);
+        //Monday start and end time
+        TextView mondayStartTime = dialogView.findViewById(R.id.mondayStartTime);
+        TextView mondayEndTime = dialogView.findViewById(R.id.mondayEndTime);
+        TextView mondayDash = dialogView.findViewById(R.id.mondayDashTextView);
+        Spinner stateSpinnerMonday = dialogView.findViewById(R.id.stateSpinnerMonday);
 
-        //I want to make it so that clicking on this will prop a time picker
-        final TextView mondayEndTime = dialogView.findViewById(R.id.mondayEndTime);
+        //Monday start and end time
+        TextView tuesdayStartTime = dialogView.findViewById(R.id.tuesdayStartTime);
+        TextView tuesdayEndTime = dialogView.findViewById(R.id.tuesdayEndTime);
+        TextView tuesdayDash = dialogView.findViewById(R.id.tuesdayDashTextView);
+        Spinner stateSpinnerTuesday = dialogView.findViewById(R.id.stateSpinnerTuesday);
 
-        final Spinner stateSpinnerMonday = dialogView.findViewById(R.id.stateSpinnerMonday);
-        final TextView mondayDash = dialogView.findViewById(R.id.mondayDashTextView);
+        //Monday start and end time
+        TextView wednesdayStartTime = dialogView.findViewById(R.id.wednesdayStartTime);
+        TextView wednesdayEndTime = dialogView.findViewById(R.id.wednesdayEndTime);
+        TextView wednesdayDash = dialogView.findViewById(R.id.wednesdayDashTextView);
+        Spinner stateSpinnerWednesday = dialogView.findViewById(R.id.stateSpinnerWednesday);
 
-        //Disable if necessary for Monday
+        //Monday start and end time
+        TextView thursdayStartTime = dialogView.findViewById(R.id.thursdayStartTime);
+        TextView thursdayEndTime = dialogView.findViewById(R.id.thursdayEndTime);
+        TextView thursdayDash = dialogView.findViewById(R.id.thursdayDashTextView);
+        Spinner stateSpinnerThursday = dialogView.findViewById(R.id.stateSpinnerThursday);
+
+        //Monday start and end time
+        TextView fridayStartTime = dialogView.findViewById(R.id.fridayStartTime);
+        TextView fridayEndTime = dialogView.findViewById(R.id.fridayEndTime);
+        TextView fridayDash = dialogView.findViewById(R.id.fridayDashTextView);
+        Spinner stateSpinnerFriday = dialogView.findViewById(R.id.stateSpinnerFriday);
+
+        //Monday start and end time
+        TextView saturdayStartTime = dialogView.findViewById(R.id.saturdayStartTime);
+        TextView saturdayEndTime = dialogView.findViewById(R.id.saturdayEndTime);
+        TextView saturdayDash = dialogView.findViewById(R.id.saturdayDashTextView);
+        Spinner stateSpinnerSaturday = dialogView.findViewById(R.id.stateSpinnerSaturday);
+
+        //Monday start and end time
+        TextView sundayStartTime = dialogView.findViewById(R.id.sundayStartTime);
+        TextView sundayEndTime = dialogView.findViewById(R.id.sundayEndTime);
+        TextView sundayDash = dialogView.findViewById(R.id.sundayDashTextView);
+        Spinner stateSpinnerSunday = dialogView.findViewById(R.id.stateSpinnerSunday);
+
+        Button addtoYourServicesBtn = dialogView.findViewById(R.id.addtoYourServicesBtn);
+
+
+        //Disable if times if closed for Monday
         disableTextViewsIfNecessary(stateSpinnerMonday,mondayStartTime,mondayDash,mondayEndTime);
+        //Choose start time and end time for Monday using time picker
+        chooseTime(mondayStartTime); chooseTime(mondayEndTime);
 
-        //Choose start time and end time for Monday
-        chooseTime(mondayStartTime);
-        chooseTime(mondayEndTime);
+
+
+        //Disable if times if closed for Tuesday
+        disableTextViewsIfNecessary(stateSpinnerTuesday,tuesdayStartTime,tuesdayDash,tuesdayEndTime);
+        //Choose start time and end time for Monday using time picker
+        chooseTime(tuesdayStartTime); chooseTime(tuesdayEndTime);
+
+        //Disable if times if closed for Wednesday
+        disableTextViewsIfNecessary(stateSpinnerWednesday,wednesdayStartTime,wednesdayDash,wednesdayEndTime);
+        //Choose start time and end time for Monday using time picker
+        chooseTime(wednesdayStartTime); chooseTime(wednesdayEndTime);
+
+        //Disable if times if closed for Thursday
+        disableTextViewsIfNecessary(stateSpinnerThursday,thursdayStartTime,thursdayDash,thursdayEndTime);
+        //Choose start time and end time for Monday using time picker
+        chooseTime(thursdayStartTime); chooseTime(thursdayEndTime);
+
+        //Disable if times if closed for Friday
+        disableTextViewsIfNecessary(stateSpinnerFriday,fridayStartTime,fridayDash,fridayEndTime);
+        //Choose start time and end time for Monday using time picker
+        chooseTime(fridayStartTime); chooseTime(fridayEndTime);
+
+        //Disable if times if closed for Saturday
+        disableTextViewsIfNecessary(stateSpinnerSaturday,saturdayStartTime,saturdayDash,saturdayEndTime);
+        //Choose start time and end time for Monday using time picker
+        chooseTime(saturdayStartTime); chooseTime(saturdayEndTime);
+
+        //Disable if times if closed for Sunday
+        disableTextViewsIfNecessary(stateSpinnerSunday,sundayStartTime,sundayDash,sundayEndTime);
+        //Choose start time and end time for Monday using time picker
+        chooseTime(sundayStartTime); chooseTime(sundayEndTime);
+
+
+        addtoYourServicesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Verify the times are correct
+
+                //Add to DB
+            }
+        });
+
+
 
 
 
@@ -147,6 +235,7 @@ public class ServiceSearchActivity extends AppCompatActivity {
 
     //This class disables the edit texts for the time if user selected closed
     public void disableTextViewsIfNecessary(Spinner spinner, final TextView startTime, final TextView dash, final TextView endTime) {
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -155,10 +244,12 @@ public class ServiceSearchActivity extends AppCompatActivity {
                     startTime.setVisibility(View.VISIBLE);
                     dash.setVisibility(View.VISIBLE);
                     endTime.setVisibility(View.VISIBLE);
+
                 } else {
                     startTime.setVisibility(View.INVISIBLE);
                     dash.setVisibility(View.INVISIBLE);
                     endTime.setVisibility(View.INVISIBLE);
+
                 }
             }
 
@@ -170,8 +261,47 @@ public class ServiceSearchActivity extends AppCompatActivity {
     }
 
     //This will create a time picker object to choose start time for a specific day
-    public void chooseTime(final TextView startTime) {
-        startTime.setOnClickListener(new View.OnClickListener() {
+    public void chooseTime(final TextView time) {
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //We want it that clicking this will show the time picker
+            public void onClick(View v) {
+
+                //Initialize the time picker dialog
+                timePickerDialog = new TimePickerDialog(ServiceSearchActivity.this,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+
+                        String hourValue = Integer.toString(hourOfDay);
+                        String minuteValue = Integer.toString(minutes);
+                        String amPM;
+                        if (hourOfDay >=12) {
+                            amPM = "PM";
+                        } else {
+                            amPM = "AM";
+                            if (hourOfDay < 10) {
+                                hourValue = "0"+hourOfDay;
+                            }
+                        }
+                        if (minutes < 10) {
+                            minuteValue = "0"+minutes;
+                        }
+                        time.setText(hourValue+":"+minuteValue+amPM);
+
+                    }
+                }, 0,0, false);
+                timePickerDialog.show();
+
+            }
+        });
+    }
+
+    public void chooseEndTime(String startHour, final TextView endTime) {
+
+//        int hour = Integer.parseInt(startT);
+
+
+        endTime.setOnClickListener(new View.OnClickListener() {
             @Override
             //We want it that clicking this will show the time picker
             public void onClick(View v) {
@@ -180,24 +310,35 @@ public class ServiceSearchActivity extends AppCompatActivity {
                 TimePickerDialog timePickerDialog = new TimePickerDialog(ServiceSearchActivity.this,new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
-                        String amPM = "";
-                        if (hourOfDay >= 12) {
+
+                        String hourValue = Integer.toString(hourOfDay);
+                        String minuteValue = Integer.toString(minutes);
+                        String amPM;
+                        if (hourOfDay >=12) {
                             amPM = "PM";
                         } else {
+                            hourValue = "0"+hourOfDay;
                             amPM = "AM";
                         }
-
-                        String minuteValue = Integer.toString(minutes);
                         if (minutes < 10) {
                             minuteValue = "0"+minutes;
                         }
-                        startTime.setText(hourOfDay+":"+minuteValue+amPM);
+                        endTime.setText(hourValue+":"+minuteValue+amPM);
+
+
                     }
                 }, 0,0, false);
                 timePickerDialog.show();
             }
         });
     }
+
+
+
+
+
+
+
 
 
 
