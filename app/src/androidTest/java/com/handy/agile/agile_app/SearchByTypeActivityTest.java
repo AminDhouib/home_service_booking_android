e1abf189ae5bd5d8226cf3babf49e5a8e48581a8
AminDhouib <adhou064@uottawa.ca>
2018-12-01 13:50:46 -0500

    Created SearchByTypeActivity test and added 1 test case.

---
+++ app/src/androidTest/java/com/handy/agile/agile_app/SearchByTypeActivityTest.java
@@ -0,0 +1,45 @@
+package com.handy.agile.agile_app;
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+
+import com.handy.agile.agile_app.HomeOwnerAccountActivities.SearchByTypeActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class SearchByTypeActivityTest {
+
+    @Rule
+    public ActivityTestRule<SearchByTypeActivity> myActivityTestRule = new ActivityTestRule<SearchByTypeActivity>(SearchByTypeActivity.class);
+    private SearchByTypeActivity searchByTypeActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        searchByTypeActivity = myActivityTestRule.getActivity();
+    }
+
+
+    @Test
+    public void EmptySpaceTest() {
+        ViewInteraction editText = onView(withId(R.id.whatToSearchType)).perform(typeText(""),closeSoftKeyboard());
+        onView(withId(R.id.applySearchTypeButton)).perform(click());
+        editText.check(matches(hasErrorText("Please enter a type!")));
+    }
+
+
+}
+
